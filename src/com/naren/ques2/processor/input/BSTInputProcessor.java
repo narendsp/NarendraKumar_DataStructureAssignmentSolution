a859d2ea0e5a4828959035a646f099563f68bfc2
narendsp <87570716+narendsp@users.noreply.github.com>
2021-10-21 19:18:37 +0530

    Add files via upload

---
+++ src/com/naren/ques2/processor/input/BSTInputProcessor.java
@@ -0,0 +1,18 @@
+package com.naren.ques2.processor.input;
+
+public class BSTInputProcessor {
+
+    private Node root;
+
+    public Node getBSTRoot(){
+        return root;
+    }
+
+    public void createBST() {
+        root = new Node(50);
+        root.setLeftNode(new Node(30));
+        root.setRightNode(new Node(60));
+        root.getLeftNode().setLeftNode(new Node(10));
+        root.getLeftNode().setRightNode(new Node(40));
+    }
+}
